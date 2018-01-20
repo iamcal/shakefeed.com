55c070b05d9b10ad43b21e24f80cb0fc6b76998f
Cal Henderson <cal@iamcal.com>
2018-01-20 22:37:46 +0000

    need to chown the crontab to root

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ln -s /var/www/html/shakefeed.com/site.conf /etc/apache2/sites-available/shakefeed.com.conf
+chown root:root /var/www/html/shakefeed.com/crontab
+chmod g-w /var/www/html/shakefeed.com/crontab
 ln -s /var/www/html/shakefeed.com/crontab /etc/cron.d/shakefeed
 a2ensite shakefeed.com
 service apache2 reload
