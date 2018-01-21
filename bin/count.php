97dac38b6a8213b7321bb009a7f9a619ba8e22aa
Cal Henderson <cal@iamcal.com>
2018-01-21 23:11:59 +0000

    all done with

--- bin/count.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include(__DIR__.'/../include/init.php');
-
-	$ret = db_fetch("SELECT COUNT(*) FROM tweets WHERE link LIKE 'http://t.co/%' OR link LIKE 'https://t.co/%'");
-	$num = array_pop($ret['rows'][0]);
-
-	echo number_format($num)."\n";
